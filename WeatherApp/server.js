79ca8af18d6e9a4b0da3f9181aef08f1ed377e0d
rkanchib <rkanchib@iu.edu>
2017-09-02 17:22:16 -0500

    Heroku added

--- WeatherApp/server.js
+++ WeatherApp/server.js
@@ -4,14 +4,24 @@ const path = require('path');
 
 //create app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req,res,next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  }else{
+    res.redirect('http://'+req.hostname+req.url);
+  }
+});
+
 
 app.use(express.static('public'));
 
+//
+// app.get('*', function (request, response){
+//   response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+// });
 
-app.get('*', function (request, response){
-  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-});
-
-app.listen(3000, function(){
-  console.log('Port is up on 3000');
+app.listen(PORT, function(){
+  console.log('Port is up on the port'+PORT);
 });
